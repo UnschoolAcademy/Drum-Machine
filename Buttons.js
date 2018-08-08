1f9a8ad20f5a9017939d729efd93a585b00f36ee
David Bond <31566213+UnschoolAcademy@users.noreply.github.com>
2018-08-08 09:08:44 -0400

    Give a specific file for each buttonObject

--- Buttons.js
+++ Buttons.js
@@ -3,19 +3,27 @@ import style from './Buttons.css';
 
 const buttonsArray = [
 {name: "Q",
+fileName: "/snares/brass-sd.wav"},
+{name: "W",
+fileName: "/snares/brass-sd2.wav"},
+{name: "E",
+fileName: "/snares/cross-stick-sd.wav"},
+{name: "A",
+fileName: "/snares/ellis_vanghoul-porno_snare.wav"},
+{name: "S",
+fileName: "/kicks/big-ol-yamaha-kick.wav"},
+{name: "D",
+fileName: "/crashes/crash-17incher.wav"},
+{name: "Z",
+fileName: "/crashes/strident-orient-cymbal.wav"},
+{name: "X",
 fileName: "/claps/707_HCP.WAV"},
-{name: "W"},
-{name: "E"},
-{name: "A"},
-{name: "S"},
-{name: "D"},
-{name: "Z"},
-{name: "X"},
-{name: "C"},
+{name: "C",
+fileName: "/crashes/OH_set2.wav"},
 ]
 let buttonsJSX = buttonsArray.map(buttonObject => 
-  <div id={buttonObject.name} className = {style['drum-pad']}>
-  <audio controls src={"./drum-sounds/"+buttonObject.fileName}></audio>{buttonObject.name}</div>);
+  <div id={buttonObject.name} className = {style['drum-pad']}>{buttonObject.name}
+  <audio src={"./drum-sounds/"+buttonObject.fileName}></audio></div>);
 
 class Buttons extends React.Component {
   constructor(props) {
