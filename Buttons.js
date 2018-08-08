6b00a5c2e022b9c4db71dde625eb6f9276bbee0b
David Bond <31566213+UnschoolAcademy@users.noreply.github.com>
2018-08-08 08:56:11 -0400

    Make it so each button will get mapped with individual files for their sounds.

--- Buttons.js
+++ Buttons.js
@@ -2,7 +2,8 @@ import React from 'react';
 import style from './Buttons.css';
 
 const buttonsArray = [
-{name: "Q"},
+{name: "Q",
+fileName: "/claps/707_HCP.WAV"},
 {name: "W"},
 {name: "E"},
 {name: "A"},
@@ -12,7 +13,10 @@ const buttonsArray = [
 {name: "X"},
 {name: "C"},
 ]
-let buttonsJSX = buttonsArray.map(buttonObject => <div id={buttonObject.name} className = {style['drum-pad']}><audio controls src="./drum-sounds/kicks/big-ol-yamaha-kick.wav"></audio>{buttonObject.name}</div>);
+let buttonsJSX = buttonsArray.map(buttonObject => 
+  <div id={buttonObject.name} className = {style['drum-pad']}>
+  <audio controls src={"./drum-sounds/"+buttonObject.fileName}></audio>{buttonObject.name}</div>);
+
 class Buttons extends React.Component {
   constructor(props) {
     super(props);
